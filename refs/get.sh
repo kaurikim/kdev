0b389dc25dd326ef717a8a9b62c2215cbef29fab
김성조 <kauri@piolink.com>
2024-05-19 16:21:32 +0900

    add get.sh to get refs codes

---
+++ refs/get.sh
@@ -0,0 +1,27 @@
+
+
+git clone ssh://git.piolink.com/opt/git/repo/phci/admin-core.git
+git clone ssh://git.piolink.com/opt/git/repo/phci/bp/bp.service.ceph.git
+
+git clone ssh://git.piolink.com/opt/git/repo/phci/bp/bp.service.openstack.git
+
+git clone ssh://git.piolink.com/opt/git/repo/phci/bp/bp.service.pnet.git
+git clone git@github.com:kaurikim/go-micro.git
+git clone https://github.com/grpc-ecosystem/grpc-gateway.git
+git clone https://github.com/grpc/grpc-go.git
+git clone https://github.com/grpc/grpc-web.git
+git clone git@github.com:kaurikim/kratos.git
+
+git clone https://github.com/kubernetes/kubernetes.git
+git clone https://github.com/kubernetes/kubernetes.git
+
+git clone ssh://git.piolink.com/opt/git/repo/phci/phci-idl.git
+
+mkdir -p msv
+cd msv
+
+git clone https://github.com/hellokvn/go-grpc-api-gateway.git
+git clone https://github.com/hellokvn/go-grpc-auth-svc.git
+git clone https://github.com/hellokvn/go-grpc-order-svc.git
+git clone https://github.com/hellokvn/go-grpc-product-svc.git
+
